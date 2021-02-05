0f19b63e78d945163ef4d811bb9d5cb2d17a2432
Tad <tad0616@gmail.com>
2021-02-05 16:13:19 +0800

    修正 jquery 載入問題。

--- theme_autorun.php
+++ theme_autorun.php
@@ -1,3 +1,10 @@
 <?php
-xoops_load('XoopsFormRendererBootstrap3');
-XoopsFormRenderer::getInstance()->set(new XoopsFormRendererBootstrap3());
+use XoopsModules\Tadtools\Utility;
+if (!class_exists('XoopsModules\Tadtools\Utility')) {
+    require XOOPS_ROOT_PATH . '/modules/tadtools/preloads/autoloader.php';
+}
+Utility::get_jquery(true);
+if (file_exists(XOOPS_ROOT_PATH . "/class/xoopsform/renderer/XoopsFormRendererBootstrap3.php")) {
+    xoops_load('XoopsFormRendererBootstrap3');
+    XoopsFormRenderer::getInstance()->set(new XoopsFormRendererBootstrap3());
+}
